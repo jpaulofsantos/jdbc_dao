04decbf04d9c8f7e23a32e5532bd785782f861ec
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-02 16:31:06 -0300

    creating Seller class

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,8 +1,10 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class MainProgram {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class MainProgram {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(11, "Bob", "bob@gmail.com", new Date(), 2500.00, department);
+        System.out.println(seller);
 
 
     }
