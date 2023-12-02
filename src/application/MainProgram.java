ebf8a96864c0058719227682ea17242d362df8f1
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-02 16:50:54 -0300

    creating Factory and Dep/Seller Dao implementation (implementing their own Dao methods)

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -2,6 +2,8 @@ package application;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.sql.Connection;
 import java.util.Date;
@@ -10,6 +12,8 @@ public class MainProgram {
     public static void main(String[] args) {
 
         Connection connection = null;
+
+        SellerDao sellerDao = DaoFactory.createSellerDao(); //o programa não conhece a implementação, somente a interface. É tbm uma forma de se fazer injeção de dep sem explicitar a implementação
 
         Department department = new Department(1, "Books");
         System.out.println(department);
